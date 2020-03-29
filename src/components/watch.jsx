50dc5e080cbc949ca0738f85d23c2a96dea38427
Student_GA115 <student_ga115@Student-GA115.fios-router.home>
2020-03-28 20:01:46 -0400

    commenting

--- src/components/watch.jsx
+++ src/components/watch.jsx
@@ -5,24 +5,34 @@ class Watch extends Component {
 
     constructor(props) {
         super(props)
+    // super is an inheritance key word used to inherit methods from the parent class created within its constructor
+    // the argument passes (props) is what we are inheriting  
         this.state = {
             count: 0
         }
+    // setting our default state , count to be 0 
     }
 
     handleStart = () => {
         this.timer = setInterval(() => {
             this.setState({count: this.state.count + 1})
         },1000);
+    // we make a function because our instructions are to be called later in a button
+    // setInterval automates the actions within our function when our function is called by a time we give it 
+    // setState updates what ever state we pass to it 
+    // this.timer stores our nested function to be called later 
     };
 
     handlePause = () => {
         clearInterval(this.timer);
+        // clearInterval clears the time constraint given to any argument we pass to it with an interval in place, stopping the automation
     };
 
     handleRestart = () => {
         this.setState({count: 0});
         clearInterval(this.timer);
+    // setState is updating count to 0 which matches our default state.
+    // clearInterval is used here to stop the count after it is reset
     };
 
     render() {
